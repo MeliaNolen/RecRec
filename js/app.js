5fe5d7d01107e4b71dbb1650b68caa8c9a70b386
MeliaNolen <mnolen5670@gmail.com>
2018-03-13 23:05:17 -0400

    fixed asynchronisity issues with geolocation and weather APIs

--- js/app.js
+++ js/app.js
@@ -36,7 +36,7 @@ $(document).on("click", ".submitButton", function() {
     geoAPI = "https://maps.googleapis.com/maps/api/geocode/json?address=" + cityInput + ",+" + stateInput + "&key=AIzaSyAadbTVL7TxCXH4u9v8RpRQAJWA0pcfp-8";
     console.log(geoAPI);
 
-    $.ajax({
+    var ajaxCall = $.ajax({
         url: geoAPI,
         method: "GET",
         success: function (response) {
@@ -51,71 +51,70 @@ $(document).on("click", ".submitButton", function() {
         }
     });
     
-    console.log(lat);
-    
-    
-    var APIKey = "166a433c57516f51dfab1f7edaed8413";
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long +
-      "&units=imperial&appid=" + APIKey;
+    ajaxCall.done(function(){
+        var APIKey = "166a433c57516f51dfab1f7edaed8413";
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long +
+        "&units=imperial&appid=" + APIKey;
 
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-      success: function(response) {
-        console.log(response);
-    
-        weatherIcon = response.list[0].weather[0].icon;
-        var weatherIconLink = "http://openweathermap.org/img/w/" + weatherIcon + ".png";
-        console.log(weatherIconLink);
-
-        generalCondition = response.list[0].weather[0].main;
-        console.log("Overall conditions = " + generalCondition);
-        $(".generalWeather").html("<img id='icon' src=" + weatherIconLink + "><br>" +
-                "<p>" + generalCondition + "</p>");
-
-        cloudCover = response.list[0].clouds.all;
-        console.log("Cloud cover = " + cloudCover +"%");
-
-        cityName = response.city.name
-        console.log("City name = " + cityName)     
-
-        var rain = response.list[0].rain;
-        var snow = response.list[0].snow;
-        if ( rain && Object.keys(rain).length ) {
-            console.log("expected rain = " + rain["3h"])
-            $(".chanceOfRain").html(rain["3h"]);
-        } else{
-            console.log("There is no rain in the forecast");
-            rain = 0;
-            $(".chanceOfRain").html(rain);
-        }
-        if (snow && Object.keys(snow).length ) {
-            console.log("expected snow = " + snow["3h"])
-        } else{
-            console.log("There is no snow in the forecast");
-            snow = 0;
-        }
+        $.ajax({
+        url: queryURL,
+        method: "GET",
+        success: function(response) {
+            console.log(response);
         
+            weatherIcon = response.list[0].weather[0].icon;
+            var weatherIconLink = "http://openweathermap.org/img/w/" + weatherIcon + ".png";
+            console.log(weatherIconLink);
 
-        wind = Math.round(response.list[0].wind.speed);
-        console.log("Wind speed is " + wind + " mph");
+            generalCondition = response.list[0].weather[0].main;
+            console.log("Overall conditions = " + generalCondition);
+            $(".generalWeather").html("<img id='icon' src=" + weatherIconLink + "><br>" +
+                    "<p>" + generalCondition + "</p>");
+
+            cloudCover = response.list[0].clouds.all;
+            console.log("Cloud cover = " + cloudCover +"%");
+
+            cityName = response.city.name
+            console.log("City name = " + cityName)     
+
+            var rain = response.list[0].rain;
+            var snow = response.list[0].snow;
+            if ( rain && Object.keys(rain).length ) {
+                console.log("expected rain = " + rain["3h"])
+                $(".chanceOfRain").html(rain["3h"]);
+            } else{
+                console.log("There is no rain in the forecast");
+                rain = 0;
+                $(".chanceOfRain").html(rain);
+            }
+            if (snow && Object.keys(snow).length ) {
+                console.log("expected snow = " + snow["3h"])
+            } else{
+                console.log("There is no snow in the forecast");
+                snow = 0;
+            }
+            
+
+            wind = Math.round(response.list[0].wind.speed);
+            console.log("Wind speed is " + wind + " mph");
+            
+            maxTemperature = Math.round(response.list[0].main.temp_max);
+            console.log("The high during your outing is predicted to be " + maxTemperature + "°F");
+            
+            
+            minTemperature = Math.round(response.list[0].main.temp_min);
+            console.log("The low during your outing is predicted to be " + minTemperature + "°F");
+            $(".temperature").html("High: " + maxTemperature + "Low: " + minTemperature);
         
-        maxTemperature = Math.round(response.list[0].main.temp_max);
-        console.log("The high during your outing is predicted to be " + maxTemperature + "°F");
-        
-        
-        minTemperature = Math.round(response.list[0].main.temp_min);
-        console.log("The low during your outing is predicted to be " + minTemperature + "°F");
-        $(".temperature").html("High: " + maxTemperature + "Low: " + minTemperature);
-    
-        // Setting Indoor or Outdoor
-        if ((rain + snow < .2) && (maxTemperature < 95) && (minTemperature > 45) && (wind < 20)){
-            var outdoor = true;
-          } else {
-            var outdoor = false;
-          }
-          console.log(outdoor);
-        }
+            // Setting Indoor or Outdoor
+            if ((rain + snow < .2) && (maxTemperature < 95) && (minTemperature > 45) && (wind < 20)){
+                var outdoor = true;
+            } else {
+                var outdoor = false;
+            }
+            console.log(outdoor);
+            }
+        });
     });
 });
 
