44d413c9aed7516f1cfa583bc8d687b5b243952b
MeliaNolen <mnolen5670@gmail.com>
2018-03-15 22:19:27 -0400

    added new buttons

--- js/app.js
+++ js/app.js
@@ -31,6 +31,15 @@ $(".modal").modal()
     $(".day2").html(day2);
     $(".day3").html(day3);
     $(".day4").html(day4);
+
+    $(".toggle-btn input[type=radio]").addClass("visuallyhidden");
+            $(".toggle-btn input[type=radio]").change(function () {
+                if ($(this).attr("name")) {
+                    $(this).parent().addClass("success").siblings().removeClass("success")
+                } else {
+                    $(this).parent().toggleClass("success");
+                }
+            });
 });
 
 function initPlaces() {
